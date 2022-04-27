9cdd424d663e55a4a5618bf256ae6b22526abf89
hardcorebuddhist <im.energo@gmail.com>
2022-04-27 19:44:26 +0200

    querying relation +

--- relation-queries.js
+++ relation-queries.js
@@ -1,5 +1,7 @@
 const { user, todoitem, todolist } = require("./models");
 
+// *** LIST WITH USERS
+
 const listsWithUsers = async () => {
   const lists = await todolist.findAll({
     include: [user],
@@ -10,12 +12,16 @@ const listsWithUsers = async () => {
 
 // listsWithUsers().then((lists) => console.log(lists));
 
+// *** Get one user by id with his lists.
+
 const userUserWithList = async (id) => {
   const result = await user.findByPk(id, { include: [todolist] });
 
   return result.get({ plain: true });
 };
 // userUserWithList(2).then((user) => console.log("user by ID with lists", user));
+
+// Get important TodoItems with the name of the list they belong to
 
 const importantTodos = async () => {
   const todos = await todoitem.findAll({
@@ -26,6 +32,23 @@ const importantTodos = async () => {
   return todos.map((item) => item.get({ plain: true }));
 };
 
-importantTodos().then((todoitem) =>
-  console.log("important todoitems", todoitem)
-);
+// importantTodos().then((todoitem) =>
+//   console.log("important todoitems", todoitem)
+// );
+
+// *** Get one user by id with his lists, which also contain their belonging TodoItem's task attribute.
+
+const fullUserById = async (id) => {
+  const result = await user.findByPk(id, {
+    include: [
+      {
+        model: todolist,
+        attributes: ["name"],
+        include: { model: todoitem, attributes: ["task"] },
+      },
+    ],
+  });
+  return result.get({ plain: true });
+};
+
+fullUserById(2).then((user) => console.log("user with tasks", user));
